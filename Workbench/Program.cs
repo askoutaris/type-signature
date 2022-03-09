5c1c95c1b1207d28c003b889f4cbc025d8078f9e
Alkiviadis Skoutaris <askoutaris@novibet.com>
2022-03-09 16:31:01 +0200

    Samples updates

--- Workbench/Program.cs
+++ Workbench/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using TemplateBinder.Extensions.DependencyInjection;
 using TypeSignature;
+using TypeSignature.HashGenerators;
 
 namespace Workbench
 {
@@ -9,15 +10,34 @@ namespace Workbench
 	{
 		static void Main(string[] args)
 		{
+			DependencyInjection();
+			SimpleUsage();
+		}
+
+		private static void SimpleUsage()
+		{
+			ITypeScanner typeScanner = new TypeScanner();
+			IHashGenerator hashGenerator = new SHA512HashGenerator();
+			ISignatureBuilder signatureBuilder = new SignatureBuilder(typeScanner, hashGenerator);
+
+			string signature = signatureBuilder.GetSignature<Person>();
+
+			Console.WriteLine($"The signature of type Person is: {signature}");
+
+			Console.ReadLine();
+		}
+
+		private static void DependencyInjection()
+		{
 			// setup our DI
 			var serviceProvider = new ServiceCollection()
 				.AddTypeSignatureSHA256()
 				.BuildServiceProvider();
 
 			// resolve SignatureBuilder
-			var signatureBuilder = serviceProvider.GetService<ISignatureBuilder>();
+			ISignatureBuilder signatureBuilder = serviceProvider.GetService<ISignatureBuilder>();
 
-			var signature = signatureBuilder.GetSignature<Person>();
+			string signature = signatureBuilder.GetSignature<Person>();
 
 			Console.WriteLine($"The signature of type Person is: {signature}");
 
